9cf0ff6d419674f8a5effba52736cb0962d4005d
John Okwaro <john.okwaro@moringaschool.com>
2022-08-17 10:49:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){   
+  if (feet <= 400){
+    return 'This one is on me!';
+  }
+  else if(feet > 2500){           
+    return 'No can do.';
+  }    
+  else if (feet > 2000){    
+    return 'I will gladly take your thirty bucks.';
+  }
+       
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  const message = city === 'NYC'? 'Ok, sounds good.': 'No go.';
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+        return 'Thank you so much.';                    
+    case 'not as generous':
+        return 'Thank you.';                   
+    default:
+        return 'Bye.'               
+      
+  }
 }
